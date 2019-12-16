44680fb2be6a38bae9f2958f6948c7ab2f4efe1d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-16 01:35:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT value, "
+  "SELECT species, COUNT(species) FROM characters
+  GROUP BY species ORDER BY COUNT(species)
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
