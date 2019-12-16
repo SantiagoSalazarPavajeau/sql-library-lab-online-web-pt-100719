0c17f8792720e292ab48906782aa1b1c55c6df0e
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-16 02:01:08 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,5 +36,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM characters
   LEFT OUTER JOIN character_books ON character_books.character_id = characters.id
+  LEFT OUTER JOIN books ON books.id = character_books.book_id
+  
   "
 end
