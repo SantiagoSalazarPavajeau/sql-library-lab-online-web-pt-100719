74ccaaac7140f0ec85fc3d45eadd08bc8483ce65
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-16 01:50:32 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,7 +19,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM subgenres
-  LEFT OUTER JOIN series ON subgenres.id = series.subgenres_id
+  LEFT OUTER JOIN series ON subgenres.id = series.subgenre_id
   LEFT OUTER JOIN authors ON authors.id = series.author_id;"
 end
 
