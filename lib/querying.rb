17215cb5b41d13f158b544a29e1e977dd7d19b9f
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-16 01:42:05 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,8 +18,8 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM authors
-  INNER JOIN subgenres 
+  "SELECT authors.name, subgenres.name FROM subgenres
+  LEFT OUTER JOIN series
   GROUP BY authors.name"
 end
 
